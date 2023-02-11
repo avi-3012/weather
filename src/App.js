b1e8ce2a008869003992c2dbff54edaec1a73250
!mute <mute@MacBook-Air.local>
2023-02-11 21:04:11 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import LeftInfo from "./components/LeftInfo.js";
 import Sidebar from "./components/Sidebar";
 import Forecast from "./components/Forecast";
 
-// Add photos to github
-// fixed live clock, added dynamic backgrounds, added dynamic weather icons
-
 function App() {
   return (
     <>
